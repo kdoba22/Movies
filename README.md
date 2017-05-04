64f004705fc4c699a8fd9ee716ff1c2873efbe2d
kdoba22 <kdoba22@gmail.com>
2017-05-04 09:17:53 -0500

    update comments

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ Full Stack Web Developer project 1, movie trailer web site
 ## Code found at
 Code can be downloaded from GITHUB  at https://github.com/kdoba22/Movies
 
+## Running
+1.  download the zip file from the github repository, https://github.com/kdoba22/Movies.git
+2.  unzip the file to a folder
+3.  you will need to have [Python "version 2.7.xx" installed on your machine] (https://www.python.org/).
+4.  From the command line, run this command from the zip folder:  ''' python entertainment_center.py '''
+5.  The browser should open up with the movie trailer website.
+
+
 ## Instructions
 After code is downloaded, either double click on the fresh_tomatoes.html or right click this file and select your favorite browser.
 
--- entertainment_center.py
+++ entertainment_center.py
@@ -1,32 +1,54 @@
 import fresh_tomatoes
 import media
 
-# create instances of the Movie class passing all 4 arguments needed to create the instance correnctly
-
-rocky = media.Movie("Rocky","An unknown local boxer gets the chance  of a lifetime",
-                    "https://upload.wikimedia.org/wikipedia/en/1/18/Rocky_poster.jpg",
+# create instances of the Movie class passing all 4 arguments
+# needed to create the instance correnctly
+rocky = media.Movie("Rocky",
+                    "A local boxer gets the chance  of a lifetime",
+                    "https://upload.wikimedia.org/wikipedia/en/1/18/"
+                    "Rocky_poster.jpg",
                     "https://www.youtube.com/watch?v=PZ0a2tUh4Nc")
 
-american_history_x = media.Movie("American History X","A young skinheads transformation",
-                    "https://upload.wikimedia.org/wikipedia/en/0/0a/American_history_x_poster.jpg",
-                    "https://www.youtube.com/watch?v=XfQYHqsiN5g")
+american_history_x = media.Movie("American History X",
+                                 "A young skinheads transformation",
+                                 "https://upload.wikimedia.org/wikipedia"
+                                 "/en/0/0a/"
+                                 "American_history_x_poster.jpg",
+                                 "https://www.youtube.com/watch?v=XfQYHqsiN5g")
 
-rudy = media.Movie("Rudy","A young man's will and drive to play Notre Dame Football",
+rudy = media.Movie("Rudy",
+                   "A young man's will and drive to play Notre Dame Football",
                    "https://upload.wikimedia.org/wikipedia/en/2/29/Rudy2.jpg",
                    "https://www.youtube.com/watch?v=5HotC8sXQPg")
 
-tombstone = media.Movie("Tombstone","The story of Wyatt Earp and the 'Shootout at the OK coral'",
-                   "https://upload.wikimedia.org/wikipedia/en/7/71/Tombstoneposter.jpeg",
-                   "https://www.youtube.com/watch?v=Mtu4ThDIdoA")
+tombstone = media.Movie("Tombstone",
+                        "The story of Wyatt Earp and the"
+                        " 'Shootout at the OK coral'",
+                        "https://upload.wikimedia.org/wikipedia/en/7/71/"
+                        "Tombstoneposter.jpeg",
+                        "https://www.youtube.com/watch?v=Mtu4ThDIdoA")
 
-something_the_lord_made = media.Movie("Something the Lord Made","The story of the firstopen heart surguries",
-                   "https://upload.wikimedia.org/wikipedia/en/5/57/Something_the_Lord_Made.jpg",
-                   "https://www.youtube.com/watch?v=UmiRohBSy5Y")
+something_the_lord_made = media.Movie("Something the Lord Made",
+                                      "The story of the firstopen heart"
+                                      " surgury",
+                                      "https://upload.wikimedia.org/"
+                                      "wikipedia/en/5/57/"
+                                      "Something_the_Lord_Made.jpg",
+                                      "https://www.youtube.com/watch?v="
+                                      "UmiRohBSy5Y")
 
-march_of_the_penguins = media.Movie("March of the Penguins","The story of the great penguin march to breed",
-                   "https://upload.wikimedia.org/wikipedia/en/1/19/March_of_the_penguins_poster.jpg",
-                   "https://www.youtube.com/watch?v=L7tWNwhSocE")
-                                                                 
+march_of_the_penguins = media.Movie("March of the Penguins",
+                                    "The story of the great penguin march",
+                                    "https://upload.wikimedia.org/wikipedia"
+                                    "/en/1/19/March_of_the_penguins_poster"
+                                    ".jpg",
+                                    "https://www.youtube.com/watch?v="
+                                    "L7tWNwhSocE")
+
 # create a movie array to be used to create themovies in the html page
 
-movies = [rocky, american_history_x, rudy, tombstone, something_the_lord_made, march_of_the_penguins]
+movies = [rocky, american_history_x, rudy, tombstone, something_the_lord_made,
+          march_of_the_penguins]
+
+#dynamically generate python data structure
+fresh_tomatoes.open_movies_page(movies)
